242b2d6374e7e92c97f6338c47dcca8d335ce45a
Siddhesh Sathe <siddheshs@nvidia.com>
2019-07-31 17:37:57 +0530

    Initial commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+        name='handy-decorators',
+        version='0.0.1',
+        description='Handy decorators for day-to-day use!',
+        py_modules=['decorators'],
+        package_dir={'':'src'}
+        )
