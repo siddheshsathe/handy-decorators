78b41e0865f3c2861332281e2ed56efb75561853
Enzo Santos <enzo.santos29@hotmail.com>
2023-09-26 10:49:30 -0300

    Add typing integration to tests

--- tests/tests.py
+++ tests/tests.py
@@ -1,46 +1,47 @@
 import pytest
+import typing
 
 class TestValgrindParser(object):
-    def test_trycatch_decorator(self):
+    def test_trycatch_decorator(self) -> None:
         from src.decorators import trycatch
         @trycatch
-        def func():
-            a = []
+        def func() -> object:
+            a: list[object] = []
             return a[2]  # Index error
 
         func()
 
-    def test_timer_decorator(self):
+    def test_timer_decorator(self) -> None:
         from src.decorators import timer
         @timer
-        def func():
+        def func() -> None:
             pass
 
         func()
 
-    def test_singleton_decorator(self):
+    def test_singleton_decorator(self) -> None:
         from src.decorators import singleton
         @singleton
         class A(object):
-            def __init__(self, *args, **kwargs):
+            def __init__(self, *args, **kwargs) -> None:
                 pass
 
-        obj1 = A(name='Siddhesh')
-        obj2 = A(name='Siddhesh', lname='Sathe')
-        obj3 = A(name='Siddhesh', lname='Sathe')
+        obj1: A = A(name='Siddhesh')
+        obj2: A = A(name='Siddhesh', lname='Sathe')
+        obj3: A = A(name='Siddhesh', lname='Sathe')
         assert obj1 is not obj2, "Created objects must be different"
         assert obj2 is obj3, "Created objects must be same"
 
-    def test_run_in_thread(self):
+    def test_run_in_thread(self) -> None:
         from src.decorators import run_in_thread
         @run_in_thread
-        def display(*args, **kwargs):
+        def display(*args, **kwargs) -> None:
             pass
         display()
 
-    def test_create_n_thread(self):
+    def test_create_n_thread(self) -> None:
         from src.decorators import create_n_threads
         @create_n_threads(thread_count=2)
-        def display(*args, **kwargs):
+        def display(*args, **kwargs) -> None:
             pass
         display()
